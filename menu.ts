2e075105d3b1c4590e66e3becf78d7618ce88470
Dayveed021 <boskey021@gmail.com>
2023-01-01 15:40:25 +0100

    bole

---
+++ menu.ts
@@ -0,0 +1,54 @@
+// <div class="dishes">
+//                 <div class="dishes_con">
+//                     <div class="pln_yam_fish  wow rotateInDownRight">
+//                         <img src="./assets/images/image0.jpeg" alt="loading....">
+//                         <div class="pln_yam_fish_text">
+//                             <p>
+//                                 Plantain <br> + <br> Yam <br> + <br> Fish <br>
+//                                 <a href="https://wa.me/2348066799567?text=%20I%20want%20to%20place%20an%20order%20for%20Plantain%20Yam%20and%20Fish%20"
+//                                     target="blank_"><button>ORDER NOW</button></a>
+//                             </p>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div class="dishes_con">
+//                     <div class="pln_ptt_yam_crk wow rotateInDownLeft">
+//                         <div class="pln_ptt_yam_crk_text">
+//                             <p>
+//                                 Plantain <br> + <br> Potatoes<br> + <br> Yam <br> + <br> Croaker <br>
+//                                 <a href="https://wa.me/2348066799567?text=%20I%20want%20to%20place%20an%20order%20for%20Plantain,%20Potatoes,%20Yam%20and%20Croaker%20"
+//                                     target="blank_"><button>ORDER NOW</button></a>
+//                             </p>
+
+//                         </div>
+//                         <img src="./assets/images/image1.jpeg" alt="loading....">
+
+//                     </div>
+//                 </div>
+//                 <div class="dishes_con">
+//                     <div class="pln_ptt_yam_crr wow rotateInDownLeft">
+
+//                         <img src="./assets/images/image1.jpeg" alt="loading....">
+//                         <div class="pln_ptt_yam_crr_text">
+//                             <p>
+//                                 Plantain <br> + <br> Potatoes<br> + <br> Yam <br> + <br> Croaker <br>
+//                                 <a href="https://wa.me/2348066799567?text=%20I%20want%20to%20place%20an%20order%20for%20Plantain,%20Potatoes,%20Yam%20and%20Croaker%20"
+//                                     target="blank_"><button>ORDER NOW</button></a>
+//                             </p>
+
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div class="dishes_con">
+//                     <div class="pln_yam_ptt_otf wow rotateInDownRight">
+//                         <img src="./assets/images/bole3.jpg" alt="loading....">
+//                         <div class="pln_yam_ptt_otf_text">
+//                             <p>
+//                                 Plantain <br> + <br> Potatoes<br> + <br> Yam <br> + <br> Other fish <br>
+//                                 <a href="https://wa.me/2348066799567?text=%20I%20want%20to%20place%20an%20order%20for%20Plantain,%20Potatoes,%20Yam,%20Yam%20and%20Other%20"
+//                                     target="blank_"><button>ORDER NOW</button></a>
+//                             </p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div> -->
